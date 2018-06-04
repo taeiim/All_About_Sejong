4e769e6cf3cd4c01207724ea8c199e5b1cc9c3ac
taeiim <ti535666@gmail.com>
2018-06-04 14:37:17 +0900

    Replace address with coordinates (GasDetailActivity)

--- app/src/main/java/com/example/parktaeim/all_about_sejong/Activity/GasStationDetailActivity.java
+++ app/src/main/java/com/example/parktaeim/all_about_sejong/Activity/GasStationDetailActivity.java
@@ -1,12 +1,16 @@
 package com.example.parktaeim.all_about_sejong.Activity;
 
+import android.app.Activity;
 import android.app.FragmentManager;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Looper;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -40,7 +44,10 @@ import org.xml.sax.InputSource;
 import org.xml.sax.XMLReader;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.ParseException;
@@ -50,6 +57,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
+import javax.net.ssl.HttpsURLConnection;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
@@ -122,6 +130,7 @@ public class GasStationDetailActivity extends AppCompatActivity implements OnMap
         backIcon.setOnClickListener(v -> finish());
 
         getGasStationInfo();
+
     }
 
     private void getGasStationInfo() {
@@ -168,6 +177,8 @@ public class GasStationDetailActivity extends AppCompatActivity implements OnMap
 
                     setUpDataString(jsonObject, oilPriceArray);
 
+                    new DataLongOperationAsynchTask().execute(address_new);
+
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
@@ -188,7 +199,8 @@ public class GasStationDetailActivity extends AppCompatActivity implements OnMap
                             });
 
                             setUpTextOilPrice();
-                            changeAddressToLatLon();
+//                            changeAddressToLatLon();
+//                            setUpMap(36.1505449,-95.99164479999999);
 
                             LinearLayout contentLayout = (LinearLayout) findViewById(R.id.gasDetail_contentLayout);
                             contentLayout.setVisibility(View.VISIBLE);
@@ -349,7 +361,7 @@ public class GasStationDetailActivity extends AppCompatActivity implements OnMap
     }
 
     private void changeAddressToLatLon() {
-        Geocoder geocoder = new Geocoder(this, Locale.KOREA);
+        Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.KOREA);
         List<Address> address = null;
         System.out.println("ADDRESS isparent==" + geocoder.isPresent());
 
@@ -370,24 +382,112 @@ public class GasStationDetailActivity extends AppCompatActivity implements OnMap
         }
     }
 
-    private void setUpMap(double lat, double lon) {
+    public void setUpMap(double lat, double lon) {
         LatLng dest = new LatLng(lat, lon);
         MarkerOptions markerOptions = new MarkerOptions();
         markerOptions.position(dest);
 
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                googleMap.addMarker(markerOptions);
-                googleMap.moveCamera(CameraUpdateFactory.newLatLng(dest));
-                googleMap.animateCamera(CameraUpdateFactory.zoomTo(16));
-            }
-        });
-
+        if(googleMap!= null){
+            googleMap.addMarker(markerOptions);
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(dest,17));
+        }else {
+            Log.d("GOOGLE MAP"," IS NULL");
+        }
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         this.googleMap = googleMap;
+        googleMap.setMinZoomPreference(6);
+    }
+
+    private class DataLongOperationAsynchTask extends AsyncTask<String, Void, String[]> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String[] doInBackground(String... params) {
+            String response;
+            String address = params[0];
+            Log.d("params ADDRESS==",address);
+            try {
+                HttpDataHandler httpDataHandler = new HttpDataHandler();
+//            response = httpDataHandler.getLatLongByURL("https://maps.googleapis.com/maps/api/geocode/json?address="+address);
+                response = httpDataHandler.getLatLongByURL("https://maps.googleapis.com/maps/api/geocode/json?address="+address+"&sensor=false&key=AIzaSyC2XPfo_uu76WV8MpHqLSvAF8Mwpre_-Uw");
+//            https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY
+                Log.d("response",""+response);
+                return new String[]{response};
+            } catch (Exception e) {
+                e.printStackTrace();
+                return new String[]{"error"};
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String... result) {
+            try {
+                Log.d("RESULT =====",result[0]);
+                JSONObject jsonObject = new JSONObject(result[0]);
+
+                double lng = ((JSONArray)jsonObject.get("results")).getJSONObject(0)
+                        .getJSONObject("geometry").getJSONObject("location")
+                        .getDouble("lng");
+
+                double lat = ((JSONArray)jsonObject.get("results")).getJSONObject(0)
+                        .getJSONObject("geometry").getJSONObject("location")
+                        .getDouble("lat");
+
+                Log.d("latitude", "" + lat);
+                Log.d("longitude", "" + lng);
+
+                setUpMap(lat,lng);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    class HttpDataHandler{
+        public HttpDataHandler(){
+
+        }
+
+        public String getLatLongByURL(String requestURL){
+            URL url;
+            String response = "";
+            try {
+                url = new URL(requestURL);
+
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(15000);
+                conn.setConnectTimeout(15000);
+                conn.setRequestMethod("GET");
+                conn.setDoInput(true);
+                conn.setRequestProperty("Content-Type",
+                        "application/x-www-form-urlencoded");
+                conn.setDoOutput(true);
+                int responseCode = conn.getResponseCode();
+
+                if (responseCode == HttpsURLConnection.HTTP_OK) {
+                    String line;
+                    BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    while ((line = br.readLine()) != null) {
+                        response += line;
+                    }
+                } else {
+                    response = "";
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return response;
+        }
     }
 }
+
+
+
+
