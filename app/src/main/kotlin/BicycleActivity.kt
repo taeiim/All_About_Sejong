05aa74af12d1b66cc55c3aec3fa5db8fb33b0f1f
taeiim <ti535666@gmail.com>
2018-09-20 19:29:44 +0900

    코틀린 개발환경 설정

---
+++ app/src/main/kotlin/BicycleActivity.kt
@@ -0,0 +1,14 @@
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.god.parktaeim.all_about_sejong.R
+
+/**
+ * Created by parktaeim on 2018. 9. 20..
+ */
+
+class BicycleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bicycle);
+    }
+}
